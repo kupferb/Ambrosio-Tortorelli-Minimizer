7244478be613b28bf99c0cfff70ac530bbf07038
kupferb <mjmj4@walla.com>
2024-05-20 00:14:23 +0300

    v

--- AmbrosioTortorelliMinimizer.py
+++ AmbrosioTortorelliMinimizer.py
@@ -1,11 +1,14 @@
 #Jacob Gildenblat, 2015
 #Implementation of edge preserving smoothing by minimizing with the Ambrosio-Tortorelli appoach
 #AM scheme, using conjugate gradients
+import os
+
 import cv2, scipy
 import numpy as np
 import sys
 import scipy
 from scipy.sparse.linalg import LinearOperator
+from scipy.io import savemat
 	
 class AmbrosioTortorelliMinimizer():
 	def __init__(self, img, iterations = 1, solver_maxiterations = 10, tol = 0.1, alpha = 1000, beta = 0.01, epsilon = 0.01):
@@ -44,7 +47,9 @@ class AmbrosioTortorelliMinimizer():
 		A = LinearOperator( (size, size), matvec = self.edge_linear_operator, dtype = np.float64)
 		b = np.ones(size) * self.beta / (4 * self.epsilon)
 
-		self.edges, _ = scipy.sparse.linalg.cg(A, b, tol = self.tol, maxiter = self.maxiter)
+		self.edges, info_ = scipy.sparse.linalg.cg(A, b, tol = self.tol, maxiter = self.maxiter)
+		# if info_:
+		# 	print(info_)
 		self.edges = np.power(self.edges.reshape(*self.g.shape), 2)
 		return self.edges
 
@@ -53,7 +58,9 @@ class AmbrosioTortorelliMinimizer():
 		A = LinearOperator( (size, size), matvec = self.image_linear_operator, dtype = np.float64)
 		b = self.g.reshape(size)
 
-		self.f, _ = scipy.sparse.linalg.cg(A, b, tol = self.tol, maxiter = self.maxiter)
+		self.f, info_ = scipy.sparse.linalg.cg(A, b, tol = self.tol, maxiter = self.maxiter)
+		# if info_:
+		# 	print(info_)
 		self.f = self.f.reshape(*self.g.shape)
 		self.update_gradients()
 		return self.f
@@ -87,19 +94,51 @@ def show_image(image, name):
 	img = np.uint8(img)
 	cv2.imshow(name, img)
 
+
+def segment_images(main_ims_folder, out_dir):
+	ims = [x for x in os.listdir(main_ims_folder) if x.endswith('.jpg')]
+	for k_im,im in enumerate(ims):
+		print(f'{k_im} im')
+		# img = cv2.imread(sys.argv[1], 1)
+		img = cv2.imread(os.path.join(main_ims_folder, im))
+		result, edges = [], []
+		for channel in cv2.split(img):
+			solver = AmbrosioTortorelliMinimizer(channel, iterations = 3, tol = 0.1, solver_maxiterations = 5)
+			u, v = solver.minimize()
+			result.append(u)
+			edges.append(v)
+
+		u = cv2.merge(result)
+		v = np.maximum(*edges)
+
+		# show_image(v, "edges")
+		# show_image(u, "image")
+		# show_image(img, "original")
+		# cv2.waitKey(-1)
+		savemat(os.path.join(out_dir, f'{im[:-4]}.mat'), {'interp_sp_image':cv2.merge([u[:,:,2],u[:,:,1],u[:,:,0]]).astype(float), 'edges':v.astype(float)})
+
+
+def main_():
+	ds_type = 'bsd300'
+	if os.environ["COMPUTERNAME"] == 'BENNYK':
+		if ds_type == 'bsd300':
+			base_ims_dir = fr'C:\Study\Datasets\BSD\300\BSDS300\images\features'
+		elif ds_type == 'bsd500':
+			base_ims_dir = fr'C:\Study\Datasets\BSD\500\BSDS500\data\images\features'
+	else:
+		if ds_type == 'bsd300':
+			main_ims_folder = r"D:\DataSet\BSD\300\BSDS300\images\train"
+			base_ims_dir = fr'D:\DataSet\BSD\300\BSDS300\images\features'
+		elif ds_type == 'bsd500':
+			main_ims_folder = r"D:\DataSet\BSD\500\BSDS500\data\images\train"
+			base_ims_dir = fr'D:\DataSet\BSD\500\BSDS500\data\images\features'
+
+	for split_type in ['train', 'test']:
+		out_dir = os.path.join(base_ims_dir,f'{split_type}_AT')
+		if not os.path.isdir(out_dir):
+			os.makedirs(out_dir)
+		segment_images(main_ims_folder, out_dir)
+
+
 if __name__ == "__main__":
-	img = cv2.imread(sys.argv[1], 1)
-	result, edges = [], []
-	for channel in cv2.split(img):
-		solver = AmbrosioTortorelliMinimizer(channel, iterations = 1, tol = 0.1, solver_maxiterations = 6)
-		f, v = solver.minimize()
-		result.append(f)
-		edges.append(v)
-
-	f = cv2.merge(result)
-	v = np.maximum(*edges)
-
-	show_image(v, "edges")
-	show_image(f, "image")
-	show_image(img, "original")
-	cv2.waitKey(-1)
+	main_()
